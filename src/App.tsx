0c3ee8c8d23d10f90a46b42803a9de8baca10dab
see-simon <sea6580@gmail.com>
2026-07-11 14:23:04 +0200

    footer fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import AdminPanel from './pages/AdminPanel'
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -10,23 +10,26 @@ import ProductDetail from './pages/ProductDetail'
 import Register from './pages/Register'
 import CreatorDashboard from './pages/CreatorDashboard'
 import Orders from './pages/Orders'
-import Footer from './pages/Footer'
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/marketplace" element={<Marketplace />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/creator" element={<CreatorDashboard />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/footer" element={<Footer />} />
-      </Routes>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Navbar />
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/creator" element={<CreatorDashboard />} />
+            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   )
 }
